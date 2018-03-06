75d161c1c15f863c9290c1167b395e7e2e217290
15991 <baoben.wu@hand-china.com>
2018-03-06 14:21:21 +0800

    add demo

--- src/main/java/com/security/login/WebSecurityConfig.java
+++ src/main/java/com/security/login/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * Created by sang on 2017/1/10.
  */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     UserDetailsService customUserService() {
